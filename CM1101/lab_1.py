73f390cebfb4c7e4dbd79026f3b7d14373511646
NIRAV <shahn30@cardiff.ac.uk>
2021-06-08 10:40:11 +0100

    new

---
+++ CM1101/lab_1.py
@@ -0,0 +1,63 @@
+# Question 2
+print(2+3)
+print(4*12)
+# To the power of '**'
+print (2**8)
+
+# Question 3
+# Zero division error it should be a non zero number
+# print(5/0)
+
+# Question 4
+print(12 % 10)
+print(5 % 9)
+print( 7 % 7)
+print(0 % 7)
+print(-1 % 7)
+print(-8 % 7)
+print(-15 % 7)
+print(1 % -7)
+print(-5 % -7)
+
+# Question 5
+print(3 * 2 + 7 % 3 ** 2)
+print(3 * (2 + 7) % 3 ** 2)
+print(3 * 2 + (7 % 3) ** 2)
+print((3 * 2 + 7) % 3 ** 2)
+print(3 * (2 + (7 % 3)) ** 2)
+print(((3 * 2 + 7) % 3) ** 2)
+print(((3 * (2 + 7)) % 3) ** 2)
+
+# Question 6
+print(type(4))
+print(type(5.0))
+print(type("Six"))
+print(type(True))
+
+# Question 7
+a = 5
+b = 4
+print(a)
+print(b)
+print(a + b)
+b = 3
+print (a + b)
+
+# Question 8
+# Quesion 9
+
+# Question 10
+import math
+print(math.sqrt(4))
+
+# Question 11
+print(True)
+print(not True)
+print(False and True)
+print(False or True)
+print((not False) and (False or True))
+print( (False or True) and (False or (True and True)))
+print( ((not False) and (not True)) or ((not True) and (not False)))
+
+# Question 12
+print(4>5)
